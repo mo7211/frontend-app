7be92485c74b739b1a9f8d5893ee03daea53811b
Moritz <moritz.lill@posteo.de>
2023-03-08 21:02:56 +0100

    setup firebase

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { initializeApp } from "firebase/app";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCkS64qd0CTq7M5Uq3QFueeYmHGqDvHFdk",
+  authDomain: "m3-viewer.firebaseapp.com",
+  projectId: "m3-viewer",
+  storageBucket: "m3-viewer.appspot.com",
+  messagingSenderId: "761790519766",
+  appId: "1:761790519766:web:56277d111466b1257cec77",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 root.render(
   <React.StrictMode>
     <App />
