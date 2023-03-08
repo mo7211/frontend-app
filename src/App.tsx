9d423f499697191159ac1e910c2c5ab7deea683e
Moritz <moritz.lill@posteo.de>
2023-03-08 20:52:56 +0100

    change Icon, create routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import { BuildingViewer } from "./components/building-viewer";
+import { LoginForm } from "./components/login-form";
+import { MapViewer } from "./components/map-viewer";
 
 function App() {
-  return <h1>Hello world!</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/building" element={<BuildingViewer />} />
+        <Route path="/map" element={<MapViewer />} />
+        <Route path="/" element={<LoginForm />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
