d56db77d7f4e7ff4e951968365119607ee474ded
Moritz <moritz.lill@posteo.de>
2023-04-26 20:19:08 +0200

    implement firestore file storing

--- src/components/building/front-menu/front-menu-content/model-list-menu.tsx
+++ src/components/building/front-menu/front-menu-content/model-list-menu.tsx
@@ -19,9 +19,36 @@ export const ModelListMenu: FC = () => {
 
     input.onchange = () => {
       console.log("Input.files");
+      if (input.files && input.files.length) {
+        const file = input.files[0];
+        const newBuilding = { ...building };
+        const { name } = file;
+        const id = `${name}-${performance.now()}`;
+        const model = { name, id };
+        newBuilding.models.push(model);
+        dispatch({
+          type: "UPLOAD_MODEL",
+          payload: {
+            model,
+            file,
+            building: newBuilding,
+          },
+        });
+      }
       input.remove();
     };
     input.click();
+  };
+
+  const onDeleteModel = (id: string) => {
+    const newBuilding = { ...building };
+    const model = newBuilding.models.find((model) => model.id === id);
+    if (!model) throw new Error("model not found!");
+    newBuilding.models = newBuilding.models.filter((model) => model.id !== id);
+    dispatch({
+      type: "DELETE_MODEL",
+      payload: { model, building: newBuilding },
+    });
   };
 
   return (
@@ -29,7 +56,7 @@ export const ModelListMenu: FC = () => {
       {building.models.length ? (
         building.models.map((model) => (
           <div className="list-item" key={model.id}>
-            <IconButton>
+            <IconButton onClick={() => onDeleteModel(model.id)}>
               <DeleteIcon />
             </IconButton>
             <span className="margin-left">{model.name}</span>
